e6f6528c2afeb1b6a000af6f2206efdf2fe5484e
Antonio Andrade <antoniomaresmares001gmail.com>
2021-09-10 12:37:43 -0500

    introduction

---
+++ IntroduccionJS/js/01-variables.js
@@ -0,0 +1,15 @@
+// Variables 
+
+var producto = "jose";
+
+let producto1 = 23;
+
+const producto2 = true;
+
+
+console.log(typeof(producto));
+
+console.log(typeof(producto1));
+
+console.log(typeof(producto2));
+
